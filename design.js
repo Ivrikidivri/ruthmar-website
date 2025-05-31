185d133eb2bad764dc4bf9863f2dbaf01defda2a
Ivrikidivri <nownevergirl@gmail.com>
2025-05-31 12:27:55 +0700

    Add files via upload

---
+++ design.js
@@ -0,0 +1,6 @@
+         function generateNumber() {
+            const randomNumber = Math.floor(Math.random() *30) + 1;
+
+         document.querySelector('.js-random-number')
+            .innerHTML = (`🫵✨${randomNumber}✨🫵`);
+          }
